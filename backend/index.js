b455e126b9deb10fa5fe25b63c1959245a7abe96
Manvendra Raj Singh <manvendrarajsinghmanny@gmail.com>
2023-12-27 11:28:13 +0530

    adding the initial setup

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  return res.json({ username: username, secret: "sha256..." });
+});
+
+
+const port=3001;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
